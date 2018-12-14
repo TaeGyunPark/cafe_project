653532b281d223cae1d9f7c9318da0f2a68bdc62
user1 <user1@user1-PC>
2018-12-14 10:16:16 +0900

    2018-12-14 10:16 dto 추가

---
+++ cafe_project/src/main/java/cafe/dto/Cafe_recommendation.java
@@ -0,0 +1,43 @@
+package cafe.dto;
+
+public class Cafe_recommendation {
+
+		private int customer_no;
+	    private String customer_nick;
+		private int cafe_recommendation;
+		
+		@Override
+		public String toString() {
+			return "Cafe_recommendation [customer_no=" + customer_no + ", customer_nick=" + customer_nick
+					+ ", cafe_recommendation=" + cafe_recommendation + "]";
+		}
+
+		public int getCustomer_no() {
+			return customer_no;
+		}
+
+		public void setCustomer_no(int customer_no) {
+			this.customer_no = customer_no;
+		}
+
+		public String getCustomer_nick() {
+			return customer_nick;
+		}
+
+		public void setCustomer_nick(String customer_nick) {
+			this.customer_nick = customer_nick;
+		}
+
+		public int getCafe_recommendation() {
+			return cafe_recommendation;
+		}
+
+		public void setCafe_recommendation(int cafe_recommendation) {
+			this.cafe_recommendation = cafe_recommendation;
+		}
+		
+		
+		
+
+
+}
