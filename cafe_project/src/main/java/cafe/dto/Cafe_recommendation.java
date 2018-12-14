bdc567667803b6f947aab224d813ddda379db453
user1 <user1@user1-PC>
2018-12-14 13:39:27 +0900

    1:39 dto 수정

--- cafe_project/src/main/java/cafe/dto/Cafe_recommendation.java
+++ cafe_project/src/main/java/cafe/dto/Cafe_recommendation.java
@@ -1,41 +1,28 @@
 package cafe.dto;
 
 public class Cafe_recommendation {
-	
-	private int customer_no;
+   
+   private int cafe_no;
     private String customer_nick;
-	private int cafe_recommendation;
-	
-	@Override
-	public String toString() {
-		return "Cafe_recommendation [customer_no=" + customer_no + ", customer_nick=" + customer_nick
-				+ ", cafe_recommendation=" + cafe_recommendation + "]";
-	}
+   @Override
+   public String toString() {
+      return "Cafe_recommendation [cafe_no=" + cafe_no + ", customer_nick=" + customer_nick + "]";
+   }
+   public int getCafe_no() {
+      return cafe_no;
+   }
+   public void setCafe_no(int cafe_no) {
+      this.cafe_no = cafe_no;
+   }
+   public String getCustomer_nick() {
+      return customer_nick;
+   }
+   public void setCustomer_nick(String customer_nick) {
+      this.customer_nick = customer_nick;
+   }
+   
+   
 
-	public int getCustomer_no() {
-		return customer_no;
-	}
-
-	public void setCustomer_no(int customer_no) {
-		this.customer_no = customer_no;
-	}
-
-	public String getCustomer_nick() {
-		return customer_nick;
-	}
-
-	public void setCustomer_nick(String customer_nick) {
-		this.customer_nick = customer_nick;
-	}
-
-	public int getCafe_recommendation() {
-		return cafe_recommendation;
-	}
-
-	public void setCafe_recommendation(int cafe_recommendation) {
-		this.cafe_recommendation = cafe_recommendation;
-	}
-	
-	
-	
+   
+   
 }
